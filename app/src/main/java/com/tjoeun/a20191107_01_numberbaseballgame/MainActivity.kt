4852e99aaaa7fb8d401af6747149732c2d7d17ab
ducho <ducho5358@gmail.com>
2019-11-07 20:35:12 +0900

    ListView 연결

--- app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/MainActivity.kt
@@ -3,11 +3,17 @@ package com.tjoeun.a20191107_01_numberbaseballgame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.tjoeun.a20191107_01_numberbaseballgame.adapters.ChatAdapter
+import com.tjoeun.a20191107_01_numberbaseballgame.datas.ChatData
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : BaseActivity() {
 
     // 471 => 4,7,1
+
+    var chatList = ArrayList<ChatData>
+    var chatAdapter:ChatAdapter? = null
 
     var questionNumArray = ArrayList<Int>()
 
@@ -47,7 +53,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        chatAdapter = ChatAdapter(this, chatList)
+        chatListView.adapter = chatAdapter
     }
 
 
