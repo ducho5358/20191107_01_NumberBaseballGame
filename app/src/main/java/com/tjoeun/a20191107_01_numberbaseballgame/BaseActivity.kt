c7238e72e7f641300c5faf7e98db1021bd4a85f4
ducho <ducho5358@gmail.com>
2019-11-07 19:45:07 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/a20191107_01_numberbaseballgame/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.tjoeun.a20191107_01_numberbaseballgame
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+
+
+}
